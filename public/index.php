e7f9ecefeee98d01e1952d02b346b53d3a4a8059
WuWencheng21 <wuwencheng21@gmail.com>
2022-03-09 12:19:24 +0100

    Aggiunge il file composer.json

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+//Serve per gestire l'errore, viene genereato un testo html con un codice (404 not found)
+$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+
+
+//Questa parte deve essere sostituita con il nome della propria+
+//sottocartella dove si trova l'applicazione
+$app->setBasePath("/projects/Registrazione_esami");//da cambiare OGNI VOLTA
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/altra_pagina', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Questa è un'altra pagina");
+    return $response;
+});
+
+$app->run();
